bd9cf8fe2fcb911f38532ed3c70df170643163ad
B1tway <andrew.shukshov@gmail.com>
2022-06-08 10:34:57 +0300

    fix

---
+++ lab3/src/_virt_net_if.c
@@ -0,0 +1,260 @@
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+#include <linux/moduleparam.h>
+#include <linux/in.h>
+#include <net/arp.h>
+#include <linux/ip.h>
+#include <linux/proc_fs.h>
+#include <linux/inet.h>
+#include <linux/if_vlan.h>
+
+
+static char* link = "enp8s0";
+module_param(link, charp, 0);
+
+static char* destipv4 = "192.168.0.1";
+module_param(destipv4, charp, 0);
+
+static char* ifname = "vni%d";
+static unsigned char data[65535];
+
+static struct net_device_stats stats;
+
+static struct net_device *child = NULL;
+struct priv {
+    struct net_device *parent;
+};
+
+#define MESSAGE_SIZE 4096
+static char message[MESSAGE_SIZE];
+static size_t message_len;
+
+
+#define PROC_FILE_NAME "var2"
+
+static struct proc_dir_entry *lab3_file;
+
+static ssize_t lab3_read(struct file *file_ptr, char __user *ubuffer, size_t buf_length, loff_t *offset)
+{
+    int len = min(message_len - (size_t)*offset, buf_length);
+
+    pr_info("Proc file read\n");
+
+    if (len <= 0)
+    {
+        pr_info("All done\n");
+        return 0;
+    }
+
+    if (copy_to_user(ubuffer, message + *offset, len))
+    {
+        pr_info("Didnt copy buffer message\n");
+        return -EFAULT;
+    }
+
+    *offset += len;
+    return len;
+}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
+#define HAVE_PROC_OPS
+#endif
+
+#ifdef HAVE_PROC_OPS
+static const struct proc_ops proc_file_ops = {
+    .proc_read = lab3_read};
+#else
+static const struct file_operations proc_file_ops = {
+    .owner = THIS_MODULE,
+    .read = lab3_read};
+#endif
+
+
+static char check_frame(struct sk_buff *skb) {
+
+	unsigned char *user_data_ptr = NULL;
+    struct iphdr *ip = (struct iphdr *)skb_network_header(skb);
+    // struct 
+    int data_len = 0;
+    
+    const u32 nr_destipv4 = in_aton( destipv4 );
+    const u16 protocol = ntohs(vlan_get_protocol(skb));
+    const u32 offset = ip->ihl << 2;
+
+    printk( KERN_INFO "Checking Frame: Protocol: %x, Version: %d, daddr: %d.%d.%d.%d, listening daddr: %s\n", protocol, ip->version,
+        ntohl(ip->daddr) >> 24, (ntohl(ip->daddr) >> 16) & 0x00FF,
+        (ntohl(ip->daddr) >> 8) & 0x0000FF, (ntohl(ip->daddr)) & 0x000000FF, destipv4 );
+
+	if ( (ip->version == IPVERSION) && (ip->daddr == nr_destipv4)) {
+        // udp = (struct udphdr*)((unsigned char*)ip + (ip->ihl * 4));
+        data_len = ntohs(ip->tot_len) - offset;
+        user_data_ptr = (unsigned char *)(skb->data + offset);
+        memcpy(data, user_data_ptr, data_len);
+        data[data_len] = '\0';
+
+        const u32 saddr = ntohl(ip->saddr);
+        const u32 daddr = ntohl(ip->daddr);
+
+        char saddr_str[128];
+        int len = sprintf(saddr_str, "saddr: %d.%d.%d.%d\n",
+                saddr >> 24,
+                (saddr >> 16) & 0x00FF,
+                (saddr >> 8) & 0x0000FF,
+                (saddr) & 0x000000FF);
+
+
+        printk("Captured IPv4 packet, saddr: %d.%d.%d.%d; daddr: %d.%d.%d.%d\n",
+                saddr >> 24, (saddr >> 16) & 0x00FF,
+                (saddr >> 8) & 0x0000FF, (saddr) & 0x000000FF,
+                daddr >> 24, (daddr >> 16) & 0x00FF,
+                (daddr >> 8) & 0x0000FF, (daddr) & 0x000000FF);
+
+
+
+        if (message_len + len >= MESSAGE_SIZE) {
+            pr_info("Message buffer is full\n");
+            strcpy(message, saddr_str);
+            message_len = 0;
+        } else {
+            strcat(message, saddr_str);
+        }
+        
+        message_len += len;
+
+    	printk(KERN_INFO "Data length: %d:", data_len);
+        return 1;
+
+    }
+    return 0;
+}
+
+static rx_handler_result_t handle_frame(struct sk_buff **pskb) {
+   // if (child) {
+        
+        	if (check_frame(*pskb)) {
+                stats.rx_packets++;
+                stats.rx_bytes += (*pskb)->len;
+            }
+        (*pskb)->dev = child;
+        return RX_HANDLER_ANOTHER;
+    //}   
+    return RX_HANDLER_PASS; 
+} 
+
+static int open(struct net_device *dev) {
+    netif_start_queue(dev);
+    printk(KERN_INFO "%s: device opened", dev->name);
+    return 0; 
+} 
+
+static int stop(struct net_device *dev) {
+    netif_stop_queue(dev);
+    printk(KERN_INFO "%s: device closed", dev->name);
+    return 0; 
+} 
+
+static netdev_tx_t start_xmit(struct sk_buff *skb, struct net_device *dev) {
+    struct priv *priv = netdev_priv(dev);
+
+    if (check_frame(skb)) {
+        stats.tx_packets++;
+        stats.tx_bytes += skb->len;
+    }
+
+    if (priv->parent) {
+        skb->dev = priv->parent;
+        skb->priority = 1;
+        dev_queue_xmit(skb);
+        return 0;
+    }
+    return NETDEV_TX_OK;
+}
+
+static struct net_device_stats *get_stats(struct net_device *dev) {
+    return &stats;
+} 
+
+static struct net_device_ops net_device_ops = {
+    .ndo_open = open,
+    .ndo_stop = stop,
+    .ndo_get_stats = get_stats,
+    .ndo_start_xmit = start_xmit
+};
+
+static void setup(struct net_device *dev) {
+    int i;
+    ether_setup(dev);
+    memset(netdev_priv(dev), 0, sizeof(struct priv));
+    dev->netdev_ops = &net_device_ops;
+
+    //fill in the MAC address
+    for (i = 0; i < ETH_ALEN; i++)
+        dev->dev_addr[i] = (char)i;
+} 
+
+int __init vni_init(void) {
+    int err = 0;
+    struct priv *priv;
+    child = alloc_netdev(sizeof(struct priv), ifname, NET_NAME_UNKNOWN, setup);
+    if (child == NULL) {
+        printk(KERN_ERR "%s: allocate error", THIS_MODULE->name);
+        return -ENOMEM;
+    }
+    priv = netdev_priv(child);
+    priv->parent = __dev_get_by_name(&init_net, link); //parent interface
+    if (!priv->parent) {
+        printk(KERN_ERR "%s: no such net: %s", THIS_MODULE->name, link);
+        free_netdev(child);
+        return -ENODEV;
+    }
+    if (priv->parent->type != ARPHRD_ETHER && priv->parent->type != ARPHRD_LOOPBACK) {
+        printk(KERN_ERR "%s: illegal net type", THIS_MODULE->name); 
+        free_netdev(child);
+        return -EINVAL;
+    }
+
+    //copy IP, MAC and other information
+    memcpy(child->dev_addr, priv->parent->dev_addr, ETH_ALEN);
+    memcpy(child->broadcast, priv->parent->broadcast, ETH_ALEN);
+    if ((err = dev_alloc_name(child, child->name))) {
+        printk(KERN_ERR "%s: allocate name, error %i", THIS_MODULE->name, err);
+        free_netdev(child);
+        return -EIO;
+    }
+
+    register_netdev(child);
+    
+    lab3_file = proc_create(PROC_FILE_NAME, 0444, NULL, &proc_file_ops);
+
+    rtnl_lock();
+    netdev_rx_handler_register(priv->parent, &handle_frame, NULL);
+    rtnl_unlock();
+    printk(KERN_INFO "Module %s loaded", THIS_MODULE->name);
+    printk(KERN_INFO "%s: create link %s", THIS_MODULE->name, child->name);
+    printk(KERN_INFO "%s: registered rx handler for %s", THIS_MODULE->name, priv->parent->name);
+    return 0; 
+}
+
+void __exit vni_exit(void) {
+    struct priv *priv = netdev_priv(child);
+    if (priv->parent) {
+        rtnl_lock();
+        netdev_rx_handler_unregister(priv->parent);
+        rtnl_unlock();
+        printk(KERN_INFO "%s: unregister rx handler for %s", THIS_MODULE->name, priv->parent->name);
+    }
+
+    proc_remove(lab3_file);
+    unregister_netdev(child);
+    free_netdev(child);
+    printk(KERN_INFO "Module %s unloaded", THIS_MODULE->name); 
+} 
+
+module_init(vni_init);
+module_exit(vni_exit);
+
+MODULE_AUTHOR("Bitway");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_DESCRIPTION("Module for capturing IPv4 packets addressed to exact destination");
